7d12ae8527cc6b0c779804972fea0d73d7169e65
Charlie Miller <246399005+charlieomiller@users.noreply.github.com>
2026-08-17 15:29:24 -0400

    Add first letter flipped is always vowel

--- src/game/letterBagRandomizer.ts
+++ src/game/letterBagRandomizer.ts
@@ -25,5 +25,22 @@ export function shuffleLetterBag(
     shuffledLetterBag[randomIndex] = temp
   }
 
+  // Swap the first letter the player sees with a vowel from the bag
+  const vowels = 'AEIOUY'
+  // Don't swap if letter is already a vowel
+  if (vowels.includes(startLetterBag[startLetterBag.length - 1])) {
+    return shuffledLetterBag
+  }
+
+  const randomResult = nextRandom(rngState)
+  /*rngState = randomResult.nextState*/
+
+  const chosenVowel = vowels[Math.floor(randomResult.value * vowels.length)]
+  const vowelIndex = shuffledLetterBag.indexOf(chosenVowel)
+
+  const temp = shuffledLetterBag[startLetterBag.length - 1]
+  shuffledLetterBag[startLetterBag.length - 1] = shuffledLetterBag[vowelIndex]
+  shuffledLetterBag[vowelIndex] = temp
+
   return shuffledLetterBag
 }
